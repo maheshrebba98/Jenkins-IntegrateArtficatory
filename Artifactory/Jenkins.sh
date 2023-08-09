7f3d8d7c49b127d5d40a4e6a91a524fedef710b9
maheshrebba98 <136562552+maheshrebba98@users.noreply.github.com>
2023-08-09 17:09:56 +0530

    Update Jenkins.sh

--- Artifactory/Jenkins.sh
+++ Artifactory/Jenkins.sh
@@ -4,7 +4,7 @@
 yum install git java-1.8.0-openjdk -y
 
 # vi .bash_profile ------> find / -name java
-# export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.382.b05-1.amzn2.0.1.x86_64
+# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.20.0.8-1.amzn2.0.1.x86_64
 # PATH=$PATH:$JAVA_HOME:$HOME/bin
 # sudo su -
 # echo $PATH
@@ -13,8 +13,10 @@ yum install git java-1.8.0-openjdk -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
-#STEP-3: DOWNLOAD JENKINS 
+#STEP-3: DOWNLOAD JAVA11 AND JENKINS 
+amazon-linux-extras install java-openjdk11 -y
 yum install jenkins -y
+update-alternatives --config java
 
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
 systemctl start jenkins.service
@@ -23,5 +25,5 @@ systemctl status jenkins.service
 ##################
 # Login into Jenkins <public_ip:8080> --> install plugins --> setup --> Manage Jenkins --> global tool config --> JDK --> 
    # Name : Java
-   # JAVA_HOME: /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.382.b05-1.amzn2.0.1.x86_64
+   # JAVA_HOME: /usr/lib/jvm/java-11-openjdk-11.0.20.0.8-1.amzn2.0.1.x86_64
    # Apply and Save
